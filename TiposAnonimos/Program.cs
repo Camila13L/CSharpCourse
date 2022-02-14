fa2f5f2c2f208beac5cb790cd728e06f12560bb4
Sara Camila Laura Manzaneda <SCamilaLauraM@outlook.es>
2022-02-14 10:20:20 -0300

    Anonimos y Tuplas

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TiposAnonimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos un objeto sin crear una clase
+            var camila = new 
+            {
+                Name = "Camila",
+                Country = "Bolivia"
+            };
+            
+            Console.WriteLine($"{camila.Name} {camila.Country}");
+
+            //Desventaja NO SE PUEDE CA,MBIAR EL VALOR DE LAS PALABRAS.
+            ////Read Only
+            //Son objetos no modificales
+
+            //Arreglos anomimos
+            //Todos los objetos de la coleccion deben tener la misma estructura
+            var beers = new[]
+            {
+                new {Name = "Red", Brand = "Delirium"},
+                new {Name = "London Porter", Brand = "Fullers"}
+            };
+
+            foreach (var b in beers)
+            {
+                Console.WriteLine($" cerveza {b.Name} {b.Brand}");
+            }
+        }
+    }
+}
---
+++ Tuplas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tuplas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
